40feaf2d9a980ca2acbdeb1035884b59518ab702
SteveQiu <qiuyuliang20042005@hotmail.com>
2015-12-31 13:11:45 -0800

    accounte and note

--- note/models.py
+++ note/models.py
@@ -7,3 +7,8 @@ class Account(models.Model):
     uid = models.CharField(max_length=20)
     pwd = models.CharField(max_length=32)
     email = models.CharField(max_length=40)
+
+class Note(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=400)
